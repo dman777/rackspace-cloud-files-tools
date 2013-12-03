a146db84fcb978cf6424567073c40e3dcf94223f
dman777 <darin.hensley@gmail.com>
2013-12-03 11:32:18 -0600

    Ver. 1.5 update

--- krunch_uploader/modules/container_util.py
+++ krunch_uploader/modules/container_util.py
@@ -1,19 +1,21 @@
+import sys
 import time
 import requests
 import logging
 
 logger = logging.getLogger("krunch")
 
-def create_containers(cloud_container_list, file_container_list, 
-        authenticate, cloud_url):
-    auth_header = { "X-Auth-Token" : authenticate.token }
-    
+
+def create_containers(cloud_container_list, file_container_list,
+                      authenticate, cloud_url):
+    auth_header = {"X-Auth-Token": authenticate.token}
+
     combined_containerlist = cloud_container_list + file_container_list
     temp_containerlist = list(set(combined_containerlist))
     count = len(temp_containerlist) - len(cloud_container_list)
-    if (count >=1):
+    if count >= 1:
         msg = "Found " + str(count) + " container(s) to add!"
-        logger.info("%s", msg)
+        logger.info(msg)
         for file_container in file_container_list:
             if file_container in cloud_container_list:
                 pass
@@ -21,33 +23,55 @@ def create_containers(cloud_container_list, file_container_list,
                 url = cloud_url + file_container
                 for i in range(5):
                     try:
-                        r = requests.put(url, headers=auth_header)
+                        r = requests.put(url, headers=auth_header, timeout=20)
                         if r.status_code == 201:
-                            msg =  ('Container \"'+ file_container + '"'
+                            msg = ('Container \"' + file_container + '"'
                                    + " created and added successfully!")
-                            logger.info("%s", msg)
+                            logger.info(msg)
                             break
                         elif r.status_code == 401:
-                            msg =  ('Token invalid for Container \"'+ file_container + '"'
+                            msg = ('Token invalid for Container \"' + file_container + '"'
                                    + " creation. Renewing Token...")
-                            logger.error("%s", msg)
-                            authenticate.getToken()
-                            auth_header = { "X-Auth-Token" : authenticate.token }
+                            logger.error(msg)
+                            authenticate.get_token()
+                            auth_header = {"X-Auth-Token": authenticate.token}
                             time.sleep(2)
                             continue
-                        else: 
+                        else:
                             if i == 4:
-                                msg =  ('Failure for Container \"'+ file_container + '"'
+                                msg = ('Failure for Container \"' + file_container + '"'
                                        + " creation. This was the 5th and final try.")
-                                logger.error("%s", msg)
+                                #exit if container doesn't get cereated.
+                                #otherwise, lots of issues with missing container.
+                                #Usr can just run this again and it will add 
+                                #containers not added yet. 
+                                logger.error(msg)
+                                sys.exit()
+                            else:
+                                msg = (
+                                    'Failure for Container \"{}\" creation. The'
+                                    ' Error code is {}. Trying again...'.format(
+                                        file_container, r.status_code))
+                                logger.error(msg)
                             continue
-                    except requests.exceptions.RequestException:
-                         if i == 4: 
-                            msg =  ('Failure for Container \"'+ file_container + '"'
+                    except requests.exceptions.ConnectionError:
+                        if i == 4:
+                            msg = ('Failure for Container \"' + file_container + '"'
                                    + " creation. This was the 5th and final try.")
-                            logger.error("%s", msg)
-                         time.sleep(2)
-                         return
+                            logger.error(msg)
+                            #exit if container doesn't get cereated.
+                            #otherwise, lots of issues with missing container.
+                            #Usr can just run this again and it will add 
+                            #containers not added yet. 
+                            sys.exit()
+                        else:
+                            msg = (
+                                'Failure for API call to create  container \"{}\".'
+                                ' This is most likely a network issue.'
+                                ' Trying Again...'.format(file_container))
+                            logger.error(msg)
+                        time.sleep(2)
+                        return
     else:
         msg = "All containers exist, none need to be added."
         logger.info(msg)
--- krunch_uploader/modules/filter.py
+++ krunch_uploader/modules/filter.py
@@ -2,8 +2,10 @@ import logging
 
 class MyFilter(object):
     """Filters for log messages. We want to keep info and error messages seperated"""
+
     def __init__(self, level):
         self.__level = level
 
     def filter(self, logRecord):
-        return logRecord.levelno <= self.__level 
+        return logRecord.levelno <= self.__level
+
